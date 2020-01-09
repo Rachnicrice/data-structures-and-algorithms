65f5a78cef33bf3b5bbd755235e8300a50bc764b
Rachael Rice <Rachnicrice@gmail.com>
2020-01-08 21:26:40 -0800

    Added more tests for binary search function

--- code401challenges/src/test/java/code401challenges/BinarySearchTest.java
+++ code401challenges/src/test/java/code401challenges/BinarySearchTest.java
@@ -5,10 +5,44 @@ import org.junit.Test;
 
 public class BinarySearchTest {
 
-    @Test public void testBinarySearch () {
+    @Test public void testBinarySearchFoundValue () {
         int[] inputArray = new int[] {2, 3, 7, 9, 4};
         int inputKey = 9;
         int expected = 3;
+
+        int actual = BinarySearch.binarySearch(inputArray, inputKey);
+
+        Assert.assertTrue(expected == actual);
+    }
+
+    @Test public void testBinarySearchValueNotFound () {
+        int[] inputArray = new int[] {3, 7, 5, 2, 8};
+        int inputKey = 6;
+        int expected = -1;
+
+        int actual = BinarySearch.binarySearch(inputArray, inputKey);
+
+        Assert.assertTrue(expected == actual);
+    }
+
+    @Test public void testBinarySearchNullArray () {
+        int[] inputArray = new int[500];
+        int inputKey = 50;
+        int expected = -1;
+
+        int actual = BinarySearch.binarySearch(inputArray, inputKey);
+
+        Assert.assertTrue(expected == actual);
+    }
+
+    @Test public void testBinarySearchLongArray () {
+        int[] inputArray = new int[1000];
+        for (int i = 0; i < inputArray.length; i++) {
+            inputArray[i] = i + 1;
+        }
+
+        int inputKey = 42;
+        int expected = 41;
 
         int actual = BinarySearch.binarySearch(inputArray, inputKey);
 
