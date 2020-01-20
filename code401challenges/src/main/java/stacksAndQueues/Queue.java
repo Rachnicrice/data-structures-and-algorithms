62cd4eb74b3a2ee381f49f201fc5b98532ecf072
Rachael Rice <Rachnicrice@gmail.com>
2020-01-20 10:43:41 -0800

    Added enqueue method and tests to queue

--- code401challenges/src/main/java/stacksAndQueues/Queue.java
+++ code401challenges/src/main/java/stacksAndQueues/Queue.java
@@ -1,4 +1,26 @@
 package stacksAndQueues;
 
 public class Queue {
+    //instance variables
+    Node front;
+    Node back;
+    Node next;
+
+    //constructor function
+    Queue () {
+        this.front = null;
+        this.next = null;
+        this.back = null;
+    }
+
+    //instance methods
+    public void enqueue (String value) {
+        Node node = new Node(value);
+        if (this.front == null) {
+            this.front = node;
+        } else {
+            this.back.next = node;
+        }
+        this.back = node;
+    }
 }
--- code401challenges/src/test/java/stacksAndQueues/QueueTest.java
+++ code401challenges/src/test/java/stacksAndQueues/QueueTest.java
@@ -1,4 +1,34 @@
 package stacksAndQueues;
 
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
 public class QueueTest {
+    Queue input;
+
+    @Before
+    public void setUp () {
+        input = new Queue();
+    }
+
+    @Test
+    public void testQueueConstructor () {
+        Assert.assertEquals(null, input.front);
+    }
+
+    @Test
+    public void testEnqueue_EmptyQueue () {
+        input.enqueue("a");
+        Assert.assertEquals("a", input.front.value);
+        Assert.assertEquals("a", input.back.value);
+    }
+
+    @Test
+    public void testEnqueue_OneValueInQueue () {
+        input.enqueue("a");
+        input.enqueue("b");
+        Assert.assertEquals("a", input.front.value);
+        Assert.assertEquals("b", input.back.value);
+    }
 }
