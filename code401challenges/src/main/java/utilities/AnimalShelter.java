f669b99ff07aec065439a4ef03b052f45a62917a
Rachael Rice <Rachnicrice@gmail.com>
2020-01-21 20:23:39 -0800

    Added animal class and test

---
+++ code401challenges/src/main/java/utilities/AnimalShelter.java
@@ -0,0 +1,13 @@
+package utilities;
+
+public class AnimalShelter {
+    //instance variable
+    Animal oldest;
+    Animal newest;
+
+    //Constructor function
+    AnimalShelter () {
+        this.oldest = null;
+        this.newest = null;
+    }
+}
---
+++ code401challenges/src/test/java/utilities/AnimalTest.java
@@ -0,0 +1,23 @@
+package utilities;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class AnimalTest {
+    Animal testDog;
+    Animal testCat;
+
+    @Before
+    public void setUp () {
+        testDog = new Animal("dog");
+        testCat = new Animal("cat");
+    }
+
+    @Test
+    public void testAnimalConstructor () {
+        Assert.assertEquals("dog", testDog.animal);
+        Assert.assertEquals("cat", testCat.animal);
+    }
+
+}
