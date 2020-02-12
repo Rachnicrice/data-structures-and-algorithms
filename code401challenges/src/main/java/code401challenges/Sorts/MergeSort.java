e7d9cf3befb3aa399ad14f7afd930a5fa1375cc7
Rachael Rice <Rachnicrice@gmail.com>
2020-02-12 12:58:06 -0800

    Added code and tests for mergesort

--- code401challenges/src/main/java/code401challenges/Sorts/MergeSort.java
+++ code401challenges/src/main/java/code401challenges/Sorts/MergeSort.java
@@ -1,14 +1,52 @@
 package code401challenges.Sorts;
 
+import java.util.Arrays;
+
 public class MergeSort {
 
     public static int[] mergeSort (int[] arr) {
+        int n = arr.length;
 
+        if (n > 1) {
+            int mid = n/2;
+            int[] left = Arrays.copyOfRange(arr, 0, mid);
+            int[] right = Arrays.copyOfRange(arr, mid, n);
 
+            mergeSort(left);
+            mergeSort(right);
+
+            mergeSort(left, right, arr);
+        }
         return arr;
     }
 
     public static int[] mergeSort(int[] left, int[] right, int[] arr){
+        int i = 0;
+        int j = 0;
+        int k = 0;
+
+        while (i < left.length && j < right.length) {
+            if (left[i] <= right[j]) {
+                arr[k] = left[i];
+                i++;
+            } else {
+                arr[k] = right[j];
+                j++;
+            }
+            k++;
+        }
+
+        if (i == left.length) {
+            for (int z = k; z < arr.length; z++){
+                arr[z] = right[j];
+                j++;
+            }
+        } else {
+            for (int z = k; z < arr.length; z++){
+                arr[z] = left[i];
+                i++;
+            }
+        }
 
         return arr;
     }
--- code401challenges/src/test/java/code401challenges/Sorts/MergeSortTest.java
+++ code401challenges/src/test/java/code401challenges/Sorts/MergeSortTest.java
@@ -1,6 +1,9 @@
 package code401challenges.Sorts;
 
 import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
 
 public class MergeSortTest {
     int[] arr;
@@ -8,5 +11,28 @@ public class MergeSortTest {
     @Before
     public void setUp () {
         arr = new int[] {4, 6, 2, 8, 19, 1};
+    }
+
+    @Test
+    public void testMergesort_UniqueValues () {
+        assertArrayEquals(new int[] {1, 2, 4, 6, 8, 19}, MergeSort.mergeSort(arr));
+    }
+
+    @Test
+    public void testMergesort_DuplicateValues () {
+        arr = new int[] {1, 2, 1, 6, 6, 19};
+        assertArrayEquals(new int[] {1, 1, 2, 6, 6, 19}, MergeSort.mergeSort(arr));
+    }
+
+    @Test
+    public void testMergesort_ReverseSorted () {
+        arr = new int[] {19, 8, 6, 4, 2, 1};
+        assertArrayEquals(new int[] {1, 2, 4, 6, 8, 19}, MergeSort.mergeSort(arr));
+    }
+
+    @Test
+    public void testMergesort_NearlySorted () {
+        arr = new int[] {1, 2, 4, 6, 19, 8};
+        assertArrayEquals(new int[] {1, 2, 4, 6, 8, 19}, MergeSort.mergeSort(arr));
     }
 }
