2449ca76ef3b97517ba8c15f5dfd33d367294d24
Rachael Rice <Rachnicrice@gmail.com>
2020-02-11 20:29:26 -0800

    started coding merge sort

---
+++ code401challenges/src/main/java/code401challenges/Sorts/MergeSort.java
@@ -0,0 +1,16 @@
+package code401challenges.Sorts;
+
+public class MergeSort {
+
+    public static int[] mergeSort (int[] arr) {
+
+
+        return arr;
+    }
+
+    public static int[] mergeSort(int[] left, int[] right, int[] arr){
+
+        return arr;
+    }
+
+}
---
+++ code401challenges/src/test/java/code401challenges/Sorts/MergeSortTest.java
@@ -0,0 +1,12 @@
+package code401challenges.Sorts;
+
+import org.junit.Before;
+
+public class MergeSortTest {
+    int[] arr;
+
+    @Before
+    public void setUp () {
+        arr = new int[] {4, 6, 2, 8, 19, 1};
+    }
+}
