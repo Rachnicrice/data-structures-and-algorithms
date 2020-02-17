ef3eb5faf619ea542be6f17a3ce7296bd21d5034
Rachael Rice <Rachnicrice@gmail.com>
2020-02-17 09:15:43 -0800

    Set up empty methods for hashtable implementation

---
+++ code401challenges/src/main/java/code401challenges/hastable/Hashtable.java
@@ -0,0 +1,31 @@
+package code401challenges.hastable;
+
+public class Hashtable {
+    //instance variables
+    int size;
+
+    //constructor function
+
+
+    //instance methods
+    public void add (String key, String value) {
+
+    }
+
+    public String get (String key) {
+
+        return "";
+    }
+
+    public Boolean contains (String key) {
+
+        return false;
+    }
+
+    public int hash (String key) {
+
+        return 0;
+    }
+
+
+}
