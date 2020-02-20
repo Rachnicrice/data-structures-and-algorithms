c80db4b782ac33da827e2e80a40450bc04daca08
Rachael Rice <Rachnicrice@gmail.com>
2020-02-19 21:12:06 -0800

    implemented simplified left join using hashmaps

--- code401challenges/src/main/java/code401challenges/leftJoin/LeftJoin.java
+++ code401challenges/src/main/java/code401challenges/leftJoin/LeftJoin.java
@@ -1,9 +1,16 @@
 package code401challenges.leftJoin;
 
 import java.util.HashMap;
+import java.util.Map;
 
 public class LeftJoin {
     public static HashMap<String, String> leftJoin (HashMap<String, String> syn, HashMap<String, String> ant) {
-        
+        HashMap<String, String> thesaurus = new HashMap<>();
+
+        for (Map.Entry pair: syn.entrySet()) {
+            String key = (String) pair.getKey();
+            thesaurus.put(key, syn.get(key) + ", " + ant.get(key));
+        }
+        return thesaurus;
     }
 }
