8f692b0c62384746916eae0211860c67ea7f74f7
Rachael Rice <Rachnicrice@gmail.com>
2020-02-19 21:04:12 -0800

    Added whiteboard of leftjoin

---
+++ code401challenges/src/main/java/code401challenges/leftJoin/LeftJoin.java
@@ -0,0 +1,9 @@
+package code401challenges.leftJoin;
+
+import java.util.HashMap;
+
+public class LeftJoin {
+    public static HashMap<String, String> leftJoin (HashMap<String, String> syn, HashMap<String, String> ant) {
+        
+    }
+}
