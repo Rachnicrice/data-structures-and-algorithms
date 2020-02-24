b76f14dc49b1711dad036a0027d2ddf17f5e24b9
Rachael Rice <Rachnicrice@gmail.com>
2020-02-24 11:23:47 -0800

    Added test for graph methods addNode and size

--- code401challenges/src/test/java/code401challenges/graph/GraphTest.java
+++ code401challenges/src/test/java/code401challenges/graph/GraphTest.java
@@ -1,4 +1,43 @@
 package code401challenges.graph;
 
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
 public class GraphTest {
+    Graph<Integer> graph;
+    GraphNode<Integer> node;
+    GraphEdge<Integer> edge;
+
+    @Before
+    public void setUp () {
+        graph = new Graph<>();
+        node = new GraphNode<>(6);
+        edge = new GraphEdge<>(node, 2);
+    }
+
+    @Test
+    public void testAddNode () {
+        graph.addNode(2);
+        int value = graph.map.getFirst().value;
+        assertEquals(2, value);
+    }
+
+    @Test
+    public void testAddEdge () {
+        graph.addNode(2);
+        graph.addEdge(graph.map.getFirst(), node, 4);
+    }
+
+    @Test
+    public void testGetNodes () {
+
+    }
+
+    @Test
+    public void testGetSize () {
+        graph.addNode(1);
+        assertEquals(1, graph.size());
+    }
 }
