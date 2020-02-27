66b72fffe669d6de71fd6b6ff3d409f72788aa92
Rachael Rice <Rachnicrice@gmail.com>
2020-02-27 09:54:53 -0800

    started depth first traversal

--- code401challenges/src/main/java/code401challenges/DepthFirst/DepthFirst.java
+++ code401challenges/src/main/java/code401challenges/DepthFirst/DepthFirst.java
@@ -4,12 +4,35 @@ import code401challenges.graph.GraphEdge;
 import code401challenges.graph.GraphNode;
 
 import java.util.LinkedList;
+import java.util.Stack;
 
 public class DepthFirst {
 
     public static LinkedList<GraphNode<String>> DepthFirst (LinkedList<GraphEdge<String>> edges) {
         LinkedList<GraphNode<String>> traversedNodes = new LinkedList<>();
-        
+        Stack<GraphNode<String>> visited = new Stack<>();
+
+        GraphNode<String> current = edges.getFirst().node;
+        visited.add(current);
+
+        while (!visited.isEmpty()) {
+            LinkedList<GraphEdge<String>> neighbors = current.neighbors;
+            int counter = 0;
+            for (int i = 0; i < neighbors.size(); i++) {
+                if (!visited.contains(neighbors.get(i).node)){
+                    visited.add(neighbors.get(i).node);
+                    current = neighbors.get(i).node;
+                    break;
+                } else {
+                    counter ++;
+                }
+            }
+
+            if (counter == neighbors.size()){
+                traversedNodes.addFirst(visited.pop());
+                current = visited.peek();
+            }
+        }
 
         return traversedNodes;
     }
--- code401challenges/src/test/java/code401challenges/DepthFirst/DepthFirstTest.java
+++ code401challenges/src/test/java/code401challenges/DepthFirst/DepthFirstTest.java
@@ -1,4 +1,39 @@
 package code401challenges.DepthFirst;
 
+import code401challenges.BreadthFirst.BreadthFirst;
+import code401challenges.graph.Graph;
+import code401challenges.graph.GraphNode;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.LinkedList;
+
+import static org.junit.Assert.assertEquals;
+
 public class DepthFirstTest {
+    Graph<String> graph;
+
+    @Before
+    public void setUp () {
+        graph = new Graph<>();
+
+        graph.addNode("a");
+        graph.addEdge(graph.map.getFirst(), new GraphNode<>("b"), "c");
+    }
+
+    @Test
+    public void testDepthFirst () {
+        LinkedList<GraphNode<String>> actual = DepthFirst.DepthFirst(graph.map.getFirst().neighbors);
+
+        for (int i = 0; i < actual.size(); i++) {
+            if (i == 0) {
+                String value = actual.get(i).value;
+                assertEquals("a", value);
+            } else if (i == 1) {
+                String value = actual.get(i).value;
+                assertEquals("c", value);
+            }
+        }
+    }
+
 }
