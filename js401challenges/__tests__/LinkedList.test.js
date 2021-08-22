565162ba9179281dedece7b502006767c0ac88b2
Rachnicrice <46403371+Rachnicrice@users.noreply.github.com>
2021-08-22 18:31:38 -0400

    added tests

---
+++ js401challenges/__tests__/LinkedList.test.js
@@ -0,0 +1,43 @@
+'use strict';
+
+const LinkedList = require('../LinkedList/LinkedList.js');
+
+describe('Linked List Class', () => {
+
+  it('can instantiate empty linked list', () => {
+    let list = new LinkedList();
+    expect(list.head).toEqual(null);
+  });
+
+  it('can add one value to the list', () => {
+    let list = new LinkedList();
+    list.insert('first');
+    expect(list.head.value).toEqual('first');
+  });
+
+  it('head node correctly points to the first node in the list', () => {
+    let list = new LinkedList()
+    list.insert('a');
+    list.insert('b');
+    expect(list.head.value).toEqual('b');
+  });
+
+  it('can insert multiple nodes into a linked lisr', () => {
+    let list = new LinkedList()
+    list.insert('a');
+    list.insert('b');
+    expect(list.head.value).toEqual('b');
+    expect(list.head.next.value).toEqual('a');
+  });
+
+  it('can turn linked list into a sting containing all values', () => {
+    let list = new LinkedList();
+    list.insert("z");
+    list.insert("w");
+    list.insert("x");
+
+    let expected = '{x} -> {w} -> {z} -> NULL';
+
+    expect(expected).toEqual(list.toString());
+  });
+})
