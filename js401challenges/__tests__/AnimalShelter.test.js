fc9b1f5308540d9a7b1a84d4fd8f12e68e837e0f
Rachnicrice <46403371+Rachnicrice@users.noreply.github.com>
2021-08-31 22:12:43 -0400

    added animal shelter

---
+++ js401challenges/__tests__/AnimalShelter.test.js
@@ -0,0 +1,13 @@
+'use strict';
+
+const AnimalShelter = require('../Stacks_Queues/AnimalShelter.js');
+
+describe('testing animal shelter', () => {
+
+  let as;
+
+  beforeEach(() => {
+    as = new AnimalShelter();
+  })
+
+});
